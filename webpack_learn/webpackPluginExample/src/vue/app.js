a81ed3a8e61ae2bcec3411632fed09742109d3e0
chenliang <chenliangxingji1@163.com>
2019-11-26 22:55:20 +0800

    keep

---
+++ webpack_learn/webpackPluginExample/src/vue/app.js
@@ -0,0 +1,9 @@
+export default{
+    template: `<div><h2>{{message}}</h2><span>{{name}}</span></div>`,
+    data() {
+        return {
+            message: "hello webpack",
+            name: "nihao"
+        }
+    }
+}
---
+++ webpack_learn/webpackPluginExample/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack=require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        //__dirname nodejs全局变量
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: 'dist/'
+    },
+
+    module: {
+        rules: [
+            {
+                // webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                            ,
+                            name: '[path][name][hash:8].[ext]'
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 name: '[path][name][hash:8].[ext]'
+            //               }
+            //         }
+            //     ]
+            // }
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        // 这里让import省略vue后缀
+        extensions:['.js','.css','.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        // 请确保引入这个插件！
+        new VueLoaderPlugin(),
+        new webpack.BannerPlugin("code by phantomold4j")
+    ]
+}
