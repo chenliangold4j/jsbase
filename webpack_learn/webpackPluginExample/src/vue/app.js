4778e66826f0e86ae7e4dd9342bc6a653bdde89d
chenliang <chenliangxingji1@163.com>
2019-11-26 23:11:26 +0800

    remove error code

--- webpack_learn/webpackPluginExample/src/vue/app.js
+++
@@ -1,9 +0,0 @@
-export default{
-    template: `<div><h2>{{message}}</h2><span>{{name}}</span></div>`,
-    data() {
-        return {
-            message: "hello webpack",
-            name: "nihao"
-        }
-    }
-}
